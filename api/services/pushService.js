1c68020be0d190ca854e3b5733f8cd4a69796661
František Žiačik <ziacik@gmail.com>
2016-01-26 22:31:37 +0100

    PushService now uses config settings. IOs support added.

--- api/services/pushService.js
+++ api/services/pushService.js
@@ -1,11 +1,40 @@
 var service = {};
 
 var gcm = require('node-gcm');
+var apn = require('apn');
+var gcmSender = null;
+var apnConnection = null;
 
-var sender = new gcm.Sender('AIzaSyAs36AYb4sGD5CTYtnBPUDhU5VBXcb2swI');
-var registrationIds = ['fuiylHj6bUw:APA91bGj7pcBPOBsBXXJ7wcXFp9hqZCABoVYxzkR51i3dWlPJgcs1D6MjU18H2Xli9gVBD-iyEnZDymmMLB5yshX3ZaAU0mnKFCOEeYb-qmOEItzfz-7uGlwVtcgy4UWP2nmTkdvFUjC'];
+if (sails.config.pushNotifications.android.senderId) {
+	gcmSender = new gcm.Sender(sails.config.pushNotifications.android.senderId);
+}
+
+if (sails.config.pushNotifications.iOs.passphrase) {
+	var options = {
+		passphrase : sails.config.pushNotifications.iOs.passphrase,
+		production : sails.config.pushNotifications.iOs.production
+	};
+
+	apnConnection = new apn.Connection(options);
+}
+
 
 var ledColor = [255, 0, 255, 255];
+
+var hasServices = function() {
+	return hasAndroidService() || hasIOsService();
+};
+
+var hasAndroidService = function() {
+	return gcmSender != null;
+};
+
+var hasIOsService = function() {
+	return apnConnection != null;
+};
+
+var hasIOsService = function() {
+};
 
 service.login = function(req, userId) {
 	var pushId = locationService.getPushId(req);
@@ -68,8 +97,11 @@ service.logout = function(req) {
 };
 
 service.push = function(message) {
+	if (!hasServices()) {
+		return;
+	}
+
 	Subscription.find().where({
-		platform : 'Android',
 		app : message.app,
 		room : message.room,
 		user : { '!' : message.sender }
@@ -79,26 +111,74 @@ service.push = function(message) {
 		}
 	
 		User.findOne(message.sender).then(function(user) {
-			var tokens = subscriptions.map(function(subscription) {
+			var androidTokens = subscriptions.filter(function(subscription) {
+				return subscription.platform === 'Android';
+			}).map(function(subscription) {
+				return subscription.identifier;
+			});
+		
+			var iOsTokens = subscriptions.filter(function(subscription) {
+				return subscription.platform === 'iOS';
+			}).map(function(subscription) {
 				return subscription.identifier;
 			});
 
-			var pushMessage = new gcm.Message();
-		
-			pushMessage.addData('title', user.username);
-			pushMessage.addData('message', message.content);
-			pushMessage.addData('ledColor', ledColor);
+			if (androidTokens.length) {
+				pushAndroid(user, message, androidTokens);
+			}
 			
-			sender.send(pushMessage, {
-				registrationTokens: tokens
-			}, function (err, response) {
-				if (err) {
-					console.error(err);
-				}
-				cleanupAndroidSubscriptions(message.app, 'Android', tokens, response);
-			});
+			if (iOsTokens.length) {
+				pushIos(user, message, iOsTokens);
+			}
 		});
 	}).done();
+};
+
+var pushAndroid = function(user, message, tokens) {
+	if (!hasAndroidService()) {
+		return;
+	}
+
+	var pushMessage = new gcm.Message();
+
+	pushMessage.addData('title', user.username);
+	pushMessage.addData('message', message.content);
+	pushMessage.addData('ledColor', ledColor);
+	
+	gcmSender.send(pushMessage, {
+		registrationTokens: tokens
+	}, function (err, response) {
+		if (err) {
+			console.error(err);
+		}
+		cleanupAndroidSubscriptions(message.app, 'Android', tokens, response);
+	});
+};
+
+var pushIos = function(user, message, tokens) {
+	if (!hasIOsService()) {
+		return false;
+	}
+
+	var feedback = new apn.Feedback(options);
+	feedback.on("feedback", function(devices) {
+		devices.forEach(function(item) {
+			// TODO clean or leave
+		});
+	});
+	
+	tokens.forEach(function(token) {
+		var myDevice = new apn.Device(token);
+		var note = new apn.Notification();
+		
+		note.expiry = Math.floor(Date.now() / 1000) + 3600; // Expires 1 hour from now.
+		note.badge = 3;
+		note.sound = "ping.aiff";
+		note.alert = "\uD83D\uDCE7 \u2709 You have a new message";
+		note.payload = {'messageFrom': user.username};
+		
+		apnConnection.pushNotification(note, myDevice);	
+	});	
 };
 
 var cleanupAndroidSubscriptions = function(appId, platform, identifiers, response) {
---
+++ config/pushNotifications.js
@@ -0,0 +1,13 @@
+/**
+ * Push Notifications configuration
+ */
+
+module.exports.pushNotifications = {
+    android : {
+	    senderId : '' /// Configure the real one in local.js
+    },
+
+    iOs : {
+    	passphrase : '',
+    	production : false
+    }};
